26f35e4e28a8d8dfd3ab90e671233ef4fb9a73db
Korhan ÖZBEK <korhanozbek44@gmail.com>
2024-09-14 23:14:22 +0300

    ecza1 tampermonkey

---
+++ scripts/ecza1TamperMonkey.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Ecza1 En Ucuz Combinasyon Bulucu
+// @version      2024-08-10
+// @description  En ucuz sepet oluşturucu
+// @match        https://www.ecza1.com/Shop/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ecza1.com
+// ==/UserScript==
+(function () {
+    'use strict';
+     const script = document.createElement('script');
+     script.src = 'https://korhanozbek44.github.io/scripts/ecza1.js';
+     document.body.append(script);
+  })();
